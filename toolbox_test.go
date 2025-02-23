c57f5eaea336441ac3235ec068a303b6cda26bae
Admin <2762713521@qq.com>
2025-02-23 21:36:07 +0800

    dev

---
+++ toolbox_test.go
@@ -0,0 +1,21 @@
+package unison
+
+import (
+	"github.com/ddkwork/golibrary/mylog"
+	"github.com/ddkwork/golibrary/stream"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestUpdateAppModule(t *testing.T) {
+	if !stream.IsDir("../toolbox") {
+		return
+	}
+	mylog.Check(os.Chdir("../toolbox"))
+	session := stream.RunCommand("git log -1 --format=\"%H\"")
+	mylog.Check(os.Chdir("../unison"))
+	id := mylog.Check2(strconv.Unquote(session.Output.String()))
+	mylog.Info("id", id)
+	stream.RunCommand("go get github.com/ddkwork/toolbox@" + id)
+}
